d65123953212896708bfbc7466754bc40bf35ddb
Luis Miguel Parent Bonilla <blessedluismi@gmail.com>
2020-07-05 00:07:18 -0400

    gitignore node modules

--- main.js
+++
@@ -1,6 +0,0 @@
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
- 
-imagemin(['assets/imgs/*.jpeg'], 'build/images', {use: [imageminJpegtran()]}).then(() => {
-    console.log('Images optimized');
-});
